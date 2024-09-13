dbffca719d2d23224fd001b4c67e7bfbd361eb53
TristanPeirce96 <tristan.peirce@dawsoncollege.qc.ca>
2024-09-13 23:39:15 +0000

    Thank you!

---
+++ TristanPeirceHW1.py
@@ -0,0 +1,4 @@
+import math
+radius = float(input("Please type the radius of your sphere here: "))
+volume = (4 / 3) * math.pi * radius**3
+print ("This is the volume of your sphere: ",(round(volume, 2)))
